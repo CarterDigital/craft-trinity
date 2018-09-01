bd32375acb288968e440bbb68273cda1a77ab068
dimasputra <dimas@carterdigital.com.au>
2018-09-02 09:17:07 +1000

    SETUP: Adding Trinity.php for plugin php file

---
+++ src/Trinity.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Trinity Plugin for Craft 3
+ *
+ * @link      https://www.carterdigital.com.au
+ * @copyright Copyright (c) 2018 Carter.
+ * @author    Dimas Putra <dimas@carterdigital.com.au>
+ */
+namespace carter\trinity;
+
+use Craft;
+use craft\base\Plugin;
+
+class Trinity extends Plugin
+{
+    public function init()
+    {
+        parent::init();
+    }
+}
